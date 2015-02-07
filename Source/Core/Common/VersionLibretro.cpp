df9e5627b4ccfe444ce1138d39d03dde48fac24a
twinaphex <libretro@gmail.com>
2015-02-07 18:59:08 +0100

    Make custom version of VersionLibretro.cpp - just stub this for now

---
+++ Source/Core/Common/VersionLibretro.cpp
@@ -0,0 +1,14 @@
+// Copyright 2013 Dolphin Emulator Project
+// Licensed under GPLv2
+// Refer to the license.txt file included.
+
+#include "Common/Common.h"
+
+const char *scm_rev_str = "Dolphin ";
+
+const char *netplay_dolphin_ver = " Libretro";
+
+const char *scm_rev_git_str = "A";
+
+const char *scm_desc_str = "B";
+const char *scm_branch_str = "C";
